11a6244205b89405fadf2843fff759aa7245edc4
Olufolajimi Ijimakinde <folajimi@Olufolajimis-MacBook-Pro.local>
2025-08-02 22:35:06 +0100

    added cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { ScrollTrigger, SplitText } from 'gsap/all';
-import gsap from 'gsap';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/cocktails";
 
-gsap .registerPlugin(ScrollTrigger, SplitText);
-
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-      <main>
+    <main>
       <Navbar />
       <Hero />
+      <Cocktails />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
